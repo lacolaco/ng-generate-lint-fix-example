a53dde28cf617deec332a9daf95399652b19075c
laco0416 <laco0416+github@gmail.com>
2017-06-01 10:07:31 +0900

    ng lint --fix

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,16 +1,16 @@
-import { DoubleQuotesPage } from './app.po';
+import { DoubleQuotesPage } from "./app.po";
 
-describe('double-quotes App', () => {
+describe("double-quotes App", () => {
   let page: DoubleQuotesPage;
 
   beforeEach(() => {
     page = new DoubleQuotesPage();
   });
 
-  it('should display welcome message', done => {
+  it("should display welcome message", done => {
     page.navigateTo();
     page.getParagraphText()
-      .then(msg => expect(msg).toEqual('Welcome to app!!'))
+      .then(msg => expect(msg).toEqual("Welcome to app!!"))
       .then(done, done.fail);
   });
 });
