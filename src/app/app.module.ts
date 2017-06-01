588d80689f16bc0d0c1a5f2c9fdf59501344c98b
laco0416 <laco0416+github@gmail.com>
2017-06-01 10:11:27 +0900

    ng g component baz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { FooComponent } from "./foo/foo.component";
 import { BarComponent } from "./bar/bar.component";
+import { BazComponent } from "./baz/baz.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     FooComponent,
-    BarComponent
+    BarComponent,
+    BazComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/baz/baz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-baz",
+  templateUrl: "./baz.component.html",
+  styleUrls: ["./baz.component.css"]
+})
+export class BazComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
