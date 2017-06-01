5ea0e260fa65c700df6a8dac16907db3a743e7f2
Angular CLI <angular-cli@angular.io>
2017-06-01 10:03:34 +0900

    chore: initial commit from @angular/cli
        _                      _                 ____ _     ___
       / \   _ __   __ _ _   _| | __ _ _ __     / ___| |   |_ _|
      / △ \ | '_ \ / _` | | | | |/ _` | '__|   | |   | |    | |
     / ___ \| | | | (_| | |_| | | (_| | |      | |___| |___ | |
    /_/   \_\_| |_|\__, |\__,_|_|\__,_|_|       \____|_____|___|
                   |___/

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,16 @@
+import { DoubleQuotesPage } from './app.po';
+
+describe('double-quotes App', () => {
+  let page: DoubleQuotesPage;
+
+  beforeEach(() => {
+    page = new DoubleQuotesPage();
+  });
+
+  it('should display welcome message', done => {
+    page.navigateTo();
+    page.getParagraphText()
+      .then(msg => expect(msg).toEqual('Welcome to app!!'))
+      .then(done, done.fail);
+  });
+});
